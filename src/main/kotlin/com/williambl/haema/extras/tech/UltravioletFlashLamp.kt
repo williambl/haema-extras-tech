34f5086215d44d096defefb8ee6de53deb1dc61f
williambl <william@williambl.com>
2022-05-09 00:38:08 +0100

    Add some effects to ultraviolet flash lamp

--- src/main/kotlin/com/williambl/haema/extras/tech/UltravioletFlashLamp.kt
+++ src/main/kotlin/com/williambl/haema/extras/tech/UltravioletFlashLamp.kt
@@ -6,24 +6,18 @@ import net.minecraft.core.particles.ParticleTypes
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResultHolder
-import net.minecraft.world.entity.Entity
+import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.LivingEntity
-import net.minecraft.world.entity.player.Player
-import net.minecraft.world.item.Item
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.UseAnim
-import net.minecraft.world.level.Level
-import net.minecraft.world.phys.AABB
-import net.minecraft.world.phys.Vec3
-import java.util.function.Predicate
-
-import com.williambl.haema.isVampire
-import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.entity.MobType
 import net.minecraft.world.entity.boss.wither.WitherBoss
 import net.minecraft.world.entity.monster.Husk
-import net.minecraft.world.entity.monster.Zombie
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.Level
+import net.minecraft.world.phys.AABB
+import net.minecraft.world.phys.Vec3
 
 class UltravioletFlashLamp(properties: Properties) : Item(properties) {
 
@@ -32,9 +26,8 @@ class UltravioletFlashLamp(properties: Properties) : Item(properties) {
     }
 
     override fun finishUsingItem(stack: ItemStack, level: Level, livingEntity: LivingEntity): ItemStack {
-        val looking = livingEntity.lookAngle
         if (level is ServerLevel) {
-            level.sendParticles(ParticleTypes.FLASH, livingEntity.x, livingEntity.getY(0.5), livingEntity.z, 5, 5.0, 5.0, 5.0, 0.3)
+            level.sendParticles(ParticleTypes.FLASH, livingEntity.x, livingEntity.getY(0.5), livingEntity.z, 5, 2.0, 2.0, 2.0, 0.3)
         }
 
         level.getEntities(null, AABB.ofSize(Vec3(livingEntity.x, livingEntity.getY(0.5), livingEntity.z), 5.0, 5.0, 5.0)) { e ->
@@ -47,8 +40,14 @@ class UltravioletFlashLamp(properties: Properties) : Item(properties) {
             }
         }
 
+        stack.getOrCreateTagElement(LAMP_DATA_TAG).putLong(FLASHING_TAG, level.gameTime)
         stack.hurtAndBreak(1, livingEntity) { e -> e.broadcastBreakEvent(EquipmentSlot.MAINHAND) }
         return stack
+    }
+
+    override fun releaseUsing(stack: ItemStack, level: Level, livingEntity: LivingEntity, timeCharged: Int) {
+        super.releaseUsing(stack, level, livingEntity, timeCharged)
+        stack.getOrCreateTagElement(LAMP_DATA_TAG).putBoolean(CHARGING_TAG, false)
     }
 
     override fun use(level: Level, player: Player, usedHand: InteractionHand): InteractionResultHolder<ItemStack> {
@@ -56,7 +55,14 @@ class UltravioletFlashLamp(properties: Properties) : Item(properties) {
         return InteractionResultHolder.consume(player.getItemInHand(usedHand))
     }
 
+    override fun onUseTick(level: Level, livingEntity: LivingEntity, stack: ItemStack, remainingUseDuration: Int) {
+        stack.getOrCreateTagElement(LAMP_DATA_TAG).putBoolean(CHARGING_TAG, true)
+    }
+
     companion object {
-        const val CHARGE_DURATION: Int = 10
+        const val CHARGE_DURATION: Int = 30
+        const val LAMP_DATA_TAG = "lampData"
+        const val FLASHING_TAG = "flashing"
+        const val CHARGING_TAG = "charging"
     }
 }
--- src/main/kotlin/com/williambl/haema/extras/tech/client/HaemaExtrasTechClient.kt
+++ src/main/kotlin/com/williambl/haema/extras/tech/client/HaemaExtrasTechClient.kt
@@ -1,8 +1,12 @@
 package com.williambl.haema.extras.tech.client
 
 import com.williambl.haema.extras.tech.HaemaExtrasTech
+import com.williambl.haema.extras.tech.HaemaExtrasTech.id
+import com.williambl.haema.extras.tech.UltravioletFlashLamp
 import net.minecraft.client.renderer.ItemBlockRenderTypes
 import net.minecraft.client.renderer.RenderType
+import net.minecraft.client.renderer.block.model.ItemOverrides
+import net.minecraft.client.renderer.item.ItemProperties
 import org.quiltmc.loader.api.ModContainer
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer
 import org.quiltmc.qsl.block.extensions.api.client.BlockRenderLayerMap
@@ -11,5 +15,21 @@ import org.quiltmc.qsl.block.extensions.mixin.client.RenderLayersMixin
 object HaemaExtrasTechClient: ClientModInitializer {
     override fun onInitializeClient(mod: ModContainer?) {
         BlockRenderLayerMap.put(RenderType.translucent(), HaemaExtrasTech.ADAPTIVE_GLASS_BLOCK)
+        ItemProperties.register(HaemaExtrasTech.ULTRAVIOLET_FLASH_LAMP, id("flashing")) { stack, level, _, _ ->
+            if (stack.getOrCreateTagElement(UltravioletFlashLamp.LAMP_DATA_TAG).getLong(UltravioletFlashLamp.FLASHING_TAG) + 2 >= (level?.gameTime ?: 0)) {
+                1.0f
+            } else {
+                0.0f
+            }
+        }
+
+        ItemProperties.register(HaemaExtrasTech.ULTRAVIOLET_FLASH_LAMP, id("charging")) { stack, level, _, _ ->
+            if (stack.getOrCreateTagElement(UltravioletFlashLamp.LAMP_DATA_TAG).getBoolean(UltravioletFlashLamp.CHARGING_TAG)) {
+                1.0f
+            } else {
+                0.0f
+            }
+        }
+
     }
 }
